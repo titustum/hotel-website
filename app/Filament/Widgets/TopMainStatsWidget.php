81c4e5e3cb8a932499c894b10b9629833fd64f14
titustum <tituskiptanuitum@gmail.com>
2026-03-06 21:24:06 +0300

    simplified

--- app/Filament/Widgets/TopMainStatsWidget.php
+++ app/Filament/Widgets/TopMainStatsWidget.php
@@ -2,6 +2,10 @@
 
 namespace App\Filament\Widgets;
 
+use App\Filament\Resources\ConferenceRooms\ConferenceRoomResource;
+use App\Filament\Resources\MenuItems\MenuItemResource;
+use App\Filament\Resources\RoomBookings\RoomBookingResource;
+use App\Filament\Resources\Rooms\RoomResource;
 use App\Models\Room;
 use App\Models\RoomBooking;
 use App\Models\MenuItem;
@@ -24,22 +28,26 @@ class TopMainStatsWidget extends StatsOverviewWidget
             Stat::make('Total Rooms', Room::count())
                 ->description('Rooms in the resort')
                 ->icon('heroicon-o-home')
-                ->color('primary'),
+                ->color('primary')
+                ->url(fn () => RoomResource::getUrl()),
 
             Stat::make('Bookings Today', RoomBooking::whereDate('check_in', today())->count())
                 ->description('Guest check-ins today')
                 ->icon('heroicon-o-calendar-days')
-                ->color('success'),
+                ->color('success')
+                ->url(fn () => RoomBookingResource::getUrl()),
 
             Stat::make('Menu Items', MenuItem::where('available', true)->count())
                 ->description('Total menu items available')
                 ->icon('heroicon-o-cake')
-                ->color('warning'),
+                ->color('warning')
+                ->url(fn () => MenuItemResource::getUrl()),
 
             Stat::make('Conference rooms', ConferenceRoom::count())
                 ->description('Total conference rooms')
                 ->icon('heroicon-o-building-office-2')
-                ->color('info'),
+                ->color('info')
+                ->url(fn () => ConferenceRoomResource::getUrl()),
         ];
     }
 }
